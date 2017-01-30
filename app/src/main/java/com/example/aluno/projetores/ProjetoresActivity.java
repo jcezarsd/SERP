3f8d3529c6960de5c804102a5e3d38177eb9b77c
Julio Cezar <dinizjcezar@gmail.com>
2017-01-30 20:23:45 -0200

    Adicionado classes padroes para utilizacao de banco de dados

---
+++ app/src/main/java/com/example/aluno/projetores/ProjetoresActivity.java
@@ -0,0 +1,18 @@
+package com.example.aluno.projetores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjetoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void buscaProjetores() {
+
+
+
+    }
+}
